e67dc4d81bc185ed09645768ba32a2daea354ec0
roman <rk-nk-sd@bk.ru>
2021-01-28 20:45:06 +0700

    Сократил html

--- src/main/java/web/controller/MainController.java
+++ src/main/java/web/controller/MainController.java
@@ -1,12 +1,18 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+
+    @GetMapping()
+    public String homePage(){
+        return "index";
+    }
 
     @RequestMapping(value = "login", method = RequestMethod.GET)
     public String loginPage() {
