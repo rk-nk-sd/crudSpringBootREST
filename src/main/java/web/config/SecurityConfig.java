f41075e4fec7b90cc30bccd2eb8f7e848255e6d3
Роман <rk-nk-sd@bk.ru>
2021-02-01 22:54:08 +0700

    Внес изменения в UserController

--- src/main/java/web/config/SecurityConfig.java
+++ src/main/java/web/config/SecurityConfig.java
@@ -53,7 +53,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // указываем URL логаута
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 // указываем URL при удачном логауте
-                .logoutSuccessUrl("/login?logout")
+//                .logoutSuccessUrl("/login?logout")
+                .logoutSuccessUrl("/login")
                 //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
                 .and().csrf().disable();
 
--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import web.service.UserService;
+import web.service.impl.UserDetailsServiceImpl;
 
 import java.security.Principal;
 
@@ -15,6 +16,8 @@ import java.security.Principal;
 @RequestMapping("/users")
 public class UsersController {
     private final UserService userService;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Autowired
     public UsersController(UserService userService) {
@@ -24,6 +27,13 @@ public class UsersController {
     @GetMapping()
     public String profile(Model model, Authentication authentication) {
         //Получим список пользователей и передадим в представление
+            UserDetails details = userDetailsService.loadUserByUsername(authentication.getName());
+            if (details != null && details.getAuthorities().stream()
+                    .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+                model.addAttribute("menu", "menuAdmin");
+            } else {
+                model.addAttribute("menu", "menuUser");
+            }
         model.addAttribute("userinfo", authentication);
         model.addAttribute("user", userService.findByUserEmail(authentication.getName()));
         return "users/index";
