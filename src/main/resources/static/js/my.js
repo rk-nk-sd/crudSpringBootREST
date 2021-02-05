65d66c1df49dd598e814959be1f8890b223f0330
roman <rk-nk-sd@bk.ru>
2021-02-05 21:01:43 +0700

    Переделал меню в админке

--- src/main/resources/static/js/my.js
+++ src/main/resources/static/js/my.js
@@ -1,3 +1,4 @@
+// HEADER
 // function auth( jQuery ) {
 //     return fetch('http://localhost:8088/admin/users/whoami')
 //         .then(response => {
@@ -8,20 +9,26 @@
 //             }
 //         })
 // }
-
+//
 // function authUserInfo(jQuery) {
-//     authUserInfo( jQuery )
+//     auth( jQuery )
 //         .then(wai => {
-//                 document.getElementById('whoami')
-//                     .innerHTML = wai.principal.username + " with roles:" + wai.authorities.map(
-//                     function (name) {
-//                         return " " + name.authority.replace("ROLE_", "")
-//                     }
-//                 );
-//                 document.getElementById()
+//
+//             document.getElementById('whoami')
+//                 .innerHTML = wai.principal.username + " with roles:" + wai.authorities.map(
+//                 function (name) {
+//                     return " " + name.authority.replace("ROLE_", "")
+//                 }
+//             );
+//
+//             if(wai.principal.username != null) {
+//                 document.getElementById('logout')
+//                     .innerHTML = '<form id="logoutForm" method="post" action="/logout"></form>'
+// // <!--                            <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>\ -->
+// //                                 < /form>'
 //             }
 //
-//         )
+//         })
 // }
-
-// $( document ).ready( authUserInfo );
+//
+// // $( document ).ready( authUserInfo );
